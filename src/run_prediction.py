a495a3d8d6847563ab565b18544fc80915ecb4a2
Marco Salvatore <masa@styx.binf.ku.dk>
2022-04-01 13:37:52 +0200

    new models and script to run prediction

--- src/run_prediction.py
+++ src/run_prediction.py
@@ -65,75 +65,31 @@ k562_chromatin = prediction_cell_line1(one_hot_seq, 'K562', 'rDHS')
 gm12878_chromatin = prediction_cell_line1(one_hot_seq, 'GM12878', 'rDHS')
 mcf7_chromatin = prediction_cell_line1(one_hot_seq, 'MCF7', 'rDHS')
 
-print('cell line promoter')
-
-a549_prom = prediction_cell_line1(one_hot_seq, 'A549', 'cage')
-hct116_prom = prediction_cell_line1(one_hot_seq, 'HCT116', 'cage')
-hepg2_prom = prediction_cell_line1(one_hot_seq, 'HEPG2', 'cage')
-k562_prom = prediction_cell_line1(one_hot_seq, 'K562', 'cage')
-gm12878_prom = prediction_cell_line1(one_hot_seq, 'GM12878', 'cage')
-
-
-print('cell line enhancer')
-
-a549_enha = prediction_cell_line1(one_hot_seq, 'A549', 'starr')
-hct116_enha = prediction_cell_line1(one_hot_seq, 'HCT116', 'starr')
-hepg2_enha = prediction_cell_line1(one_hot_seq, 'HEPG2', 'starr')
-k562_enha = prediction_cell_line1(one_hot_seq, 'K562', 'starr')
-mcf7_enha = prediction_cell_line1(one_hot_seq, 'MCF7', 'starr')
-
-
 
 open_chromatin = np.concatenate(open_chromatin, axis=0)
 
 a549_chromatin = np.concatenate(a549_chromatin, axis=0)
-a549_prom = np.concatenate(a549_prom, axis=0)
-a549_enha = np.concatenate(a549_enha, axis=0)
 
 hct116_chromatin = np.concatenate(hct116_chromatin, axis=0)
-hct116_prom = np.concatenate(hct116_prom, axis=0)
-hct116_enha = np.concatenate(hct116_enha, axis=0)
 
 hepg2_chromatin = np.concatenate(hepg2_chromatin, axis=0)
-hepg2_prom = np.concatenate(hepg2_prom, axis=0)
-hepg2_enha = np.concatenate(hepg2_enha, axis=0)
 
 k562_chromatin = np.concatenate(k562_chromatin, axis=0)
-k562_prom = np.concatenate(k562_prom, axis=0)
-k562_enha = np.concatenate(k562_enha, axis=0)
 
 gm12878_chromatin = np.concatenate(gm12878_chromatin, axis=0)
-gm12878_prom = np.concatenate(gm12878_prom, axis=0)
 
 mcf7_chromatin = np.concatenate(mcf7_chromatin, axis=0)
-mcf7_enha = np.concatenate(mcf7_enha, axis=0)
 
 
 print('creating final output file')
 
-final_pred_array = np.array([open_chromatin, 
-                             a549_chromatin, a549_prom, a549_enha,
-                             hct116_chromatin, hct116_prom, hct116_enha,
-                             hepg2_chromatin, hepg2_prom, hepg2_enha,
-                             k562_chromatin, k562_prom, k562_enha,
-                             gm12878_chromatin, gm12878_prom,
-                             mcf7_chromatin, mcf7_enha,
-                             ])
+final_pred_array = np.array([open_chromatin,  a549_chromatin, hct116_chromatin, 
+                             hepg2_chromatin, k562_chromatin, gm12878_chromatin, mcf7_chromatin])
 
-tmp_df = pd.DataFrame(final_pred_array, columns = name_seq, index = ['TL_Open_Closed', 
-                                                                     'A549_rDHS', 'A549_prom','A549_enh',
-                                                                     'HCT116_rDHS', 'HCT116_prom','HCT116_enh',
-                                                                     'HEPG2_rDHS', 'HEPG2_prom','HEPG2_enh',
-                                                                     'K562_rDHS', 'K562_prom','K562_enh',
-                                                                     'GM12878_rDHS', 'GM12878_prom',
-                                                                     'MCF7_rDHS', 'MCF7_enh',
-                                                               
-                                                                    ]).T
+tmp_df = pd.DataFrame(final_pred_array, columns = name_seq, index = ['TL_Open_Closed', 'A549', 'HCT116',  'HEPG2',  'K562',  'GM12878','MCF7' ]).T
 
-final_df = tmp_df[['TL_Open_Closed', 
-                   'A549_rDHS', 'HCT116_rDHS', 'HEPG2_rDHS', 'K562_rDHS', 'GM12878_rDHS', 'MCF7_rDHS',
-                   'A549_prom', 'HCT116_prom', 'HEPG2_prom', 'K562_prom', 'GM12878_prom',
-                   'A549_enh', 'HCT116_enh', 'HEPG2_enh', 'K562_enh', 'MCF7_enh']].round(3)
+final_df = tmp_df[['TL_Open_Closed', 'A549', 'HCT116', 'HEPG2', 'K562', 'GM12878', 'MCF7',
+                   ]].round(3)
 
 final_df.to_csv(args.output+'output.csv')
 os.remove(args.fasta)
